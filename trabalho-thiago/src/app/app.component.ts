6142a665c6ed262eaf65123f42e41f57ee019799
Danilo Francisco Martins Floridi <danilofloridiseriado@gmail.com>
2025-02-24 22:40:16 -0300

    CommitAtt

--- trabalho-thiago/src/app/app.component.ts
+++ trabalho-thiago/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatCardModule],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'trabalho-thiago';
+  imagePath = 'assets/images/carro.jpg';
 }
